ac9176f1d9590994bc83a4665042eafcefef8f08
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-06-12 10:51:10 +0530

    Implimenting Interfaces

--- BASICS_C#/Mobile_Functions/Andriod.cs
+++ BASICS_C#/Mobile_Functions/Andriod.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Mobile_Functions
 {
-    internal class Android : Mobile//inherited class
+    internal class Android : Mobile,ICamera//inherited class
     {
         public string Version;
         public string OS;
@@ -17,10 +17,19 @@ namespace Mobile_Functions
             this.Version = version;
             this.OS = os;
         }
+        public void Take_Photo()
+        {
+            Console.WriteLine("Taking a photo...");
+        }
 
         public void Print_Android_Details()
         {
             Console.WriteLine($"Brand: {Brand}, Model: {Model}, Version: {Version}, OS: {OS}, Battery Level: {BatteryLevel}%");
+        }
+
+        public void Take_photo()
+        {
+            throw new NotImplementedException();
         }
     }
 
---
+++ BASICS_C#/Mobile_Functions/ICamera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Functions
+{
+     public interface ICamera//interfaces should be given as public
+    {
+        public void Take_photo();
+    }
+
+
+}
