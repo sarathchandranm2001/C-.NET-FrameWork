68cc9d43d6428f16e91957b9086a2a3c743748fb
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-06-12 10:15:30 +0530

    First Menu Driven App

---
+++ BASICS_C#/Mobile_Functions/Mobile.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mobile_Functions
+{
+    internal class Mobile
+    {
+        public string Brand;
+        public string Model;
+        public int BatteryLevel;
+
+        public Mobile(String Brand,String Model) {
+            this.Brand = Brand;
+            this.Model = Model;
+            this.BatteryLevel = 75;
+        
+        }//initiate initial values for atrrtibutes
+
+        public void Make_Call(string phoneNumber)
+        {
+            Console.WriteLine(phoneNumber);
+            
+        }
+
+        public void Charge_Battery(int charge)
+        {
+            if (BatteryLevel == 0)
+            {
+                Console.WriteLine("No Battery ! Charge Your Mobile");
+            }
+            else if (BatteryLevel <= 10)
+            {
+                Console.WriteLine("Battery Level low !!! Charge Your Mobile");
+            }
+            else if (BatteryLevel == 100)
+            {
+                Console.WriteLine("Fully Charged");
+            }
+            else
+            {
+                Console.WriteLine($"Battery level {BatteryLevel}");
+            }
+        }
+
+        public void Use_Model()
+        {
+        }
+
+        public void Print_Details()
+        {
+
+        }
+        
+      }
+}
---
+++ BASICS_C#/Mobile_Functions/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Mobile_Functions
+{
+    internal class Mobile_Functions
+    {
+        static void Main(string[] args)
+        {
+            Mobile mobile=new Mobile("Apple","11Pro");
+            Console.WriteLine("Enter choice \t 1.Make Call \t2.Battery Level \t3.Exit");
+            int choice=int.Parse(Console.ReadLine());
+            while (choice != 3) {
+                switch (choice) {
+                case 1:
+                        Console.WriteLine("Enter PhoneNumber :");
+                        string phn=Console.ReadLine();
+                        mobile.Make_Call(phn);
+                        break;
+                case 2:
+                        Console.WriteLine("");
+                        break;
+                default: 
+                        Console.WriteLine("Enter Valid Choice :");
+                        break;
+                 
+
+
+
+                }
+
+                Console.WriteLine("Enter choice \t 1.Make Call \t2.Battery Level \t3.Exit");
+                choice=int.Parse(Console.ReadLine());
+
+            }
+
+            
+        }
+    }
+}
