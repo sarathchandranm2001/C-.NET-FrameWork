ba69dbd99692f9db6e40e566ba70898a0b5a784f
SARATH CHANDRAN M <sarathchandranm2001@gmail.com>
2024-06-25 11:41:06 +0530

    Dynamic Variables

---
+++ Advanced.Net/Dynamic_Variable_in_Generic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Dynamic_Variable_in_Generic
+{
+    internal class Program
+    {
+        public void Add<T>(T a, T b)
+        {
+            dynamic x = a;
+            dynamic y = b;
+            T sum = x + y;
+            Console.WriteLine($"Result of adding{a} and {b} :{sum}");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two values to add");
+            int x=int.Parse(Console.ReadLine());
+            int y =int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Two Strings to Concatinate");
+            string str1 = Console.ReadLine();
+            string str2 = Console.ReadLine();
+
+
+            Program program = new Program();
+
+            program.Add(x, y);
+            program.Add(str1, str2); 
+        }
+    }
+}
